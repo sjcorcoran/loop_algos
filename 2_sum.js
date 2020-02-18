a263451e19db3da86b82eb54d49dd70a264c80ea
sjcorcoran <seanjcorcoran89@gmail.com>
2020-02-18 13:22:00 -0600

    I am really getting a handle on this

--- 2_sum.js
+++ 2_sum.js
@@ -3,3 +3,9 @@
 // Num: 1, Sum: 1, Num: 2, Sum: 3, Num: 3, Sum: 6, Num: 4, Sum: 10, Num: 5, Sum: 15 .... etc
 
 // YOUR CODE HERE 
+var sum =0;
+for(var num =1; num<=10; num++){
+    sum=num+sum
+    console.log(num)
+    console.log(sum)
+}
--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -3,3 +3,33 @@
 
 
 // YOUR CODE HERE
+// example sum
+var num=0
+var sum=0
+while(num<10){
+    num++;
+    console.log(num)
+    sum=num+sum;
+    console.log(sum)
+}
+// Fizzbuzz
+var i=0
+while(i<29){
+    i++
+    if(i %3 ==0 && i %5 ==0)
+    console.log("Fizzbuzz")
+    else if( i %3 ==0)
+    console.log("Fizz")
+    else if(i %5 ==0)
+    console.log("Buzz")
+else{
+    console.log(i)
+}
+// Print odds
+var skip=0
+while (skip<20){
+    if(skip %2==1)
+    console.log(skip)
+    skip++
+}
+}
